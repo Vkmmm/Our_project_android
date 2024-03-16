09cef47e59b4c092e2b7fbb88a4e8fea3d9ae3a1
Diana <d.v4litova@yandex.ru>
2024-03-16 23:08:15 +0300

    Создание регистрации ученика

--- app/src/main/java/com/example/our_trpp_project/Student_1.java
+++ app/src/main/java/com/example/our_trpp_project/Student_1.java
@@ -1,7 +1,16 @@
 package com.example.our_trpp_project;
 
-public class Student_1 {
-    protected void OnCreate(){
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.Button;
 
+public class Student_1 {
+    protected void OnCreate(View rootView){ // Принимаем корневую View в качестве параметра
+        Button button = rootView.findViewById(R.id.button2); // Ищем кнопку внутри корневой View
+        String text = "У меня уже есть аккаунт";
+        SpannableString content = new SpannableString(text);
+        content.setSpan(new UnderlineSpan(), 0, text.length(), 0);
+        button.setText(content);
     }
 }
