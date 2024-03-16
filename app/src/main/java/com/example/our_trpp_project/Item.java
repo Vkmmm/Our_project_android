da02d802939fbc5210c2dffd56cab763bd0503bb
Diana <d.v4litova@yandex.ru>
2024-03-16 23:54:25 +0300

    Создание выбора предметов

---
+++ app/src/main/java/com/example/our_trpp_project/Item.java
@@ -0,0 +1,12 @@
+package com.example.our_trpp_project;
+
+public class Item {
+    private String text;
+    public Item(String text){
+        this.text = text;
+    }
+    public String getText(){
+        return text;
+    }
+
+}
