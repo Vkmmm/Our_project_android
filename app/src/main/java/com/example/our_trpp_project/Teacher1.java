f7aa09eab4cd531d84f3aa081b6ada5114a84910
Diana <d.v4litova@yandex.ru>
2024-03-31 20:37:02 +0300

    Регистрация репетитора

---
+++ app/src/main/java/com/example/our_trpp_project/Teacher1.java
@@ -0,0 +1,6 @@
+package com.example.our_trpp_project;
+
+import androidx.fragment.app.Fragment;
+
+public class Teacher1 extends Fragment {
+}
---
+++ app/src/main/java/com/example/our_trpp_project/TeacherRegister1.java
@@ -0,0 +1,6 @@
+package com.example.our_trpp_project;
+
+import androidx.fragment.app.Fragment;
+
+public class TeacherRegister1 extends Fragment {
+}
