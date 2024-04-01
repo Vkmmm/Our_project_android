2a002d4caefc2541d3290b72c180592afc4e7f26
Vika31 <kyklova17@gmail.com>
2024-04-01 16:14:05 +0300

    Был сделан навигационный граф

--- app/src/main/java/com/example/our_trpp_project/MainActivity.java
+++ app/src/main/java/com/example/our_trpp_project/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.our_trpp_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,17 +10,16 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    public MainActivity() {
+        super(R.layout.activity_main);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button buttonClick = findViewById(R.id.button);
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Student_1.class);
-            }
-        };
+        if (savedInstanceState == null) {
+            NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+            NavController navController = navHostFragment.getNavController();
+            navController.navigate(R.id.startFragment);
+        }
     }
 }
