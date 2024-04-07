9521a92bc070e1b167d21adf80d2470ace4014b7
Vika31 <kyklova17@gmail.com>
2024-04-07 21:36:05 +0300

    Создание папки UI

---
+++ app/src/main/java/com/example/our_trpp_project/UI/StartFragment.java
@@ -0,0 +1,37 @@
+package com.example.our_trpp_project.UI;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.our_trpp_project.R;
+
+
+public class StartFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_start, container, false);
+        Button button = view.findViewById(R.id.button_stud);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_startFragment_to_student1);
+            }
+        });
+        return view;
+    }
+}
